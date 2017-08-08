a2bf60675efa585041a06fecc58015b41c21375d
Deaglan <DeaglanLynch@hotmail.co.uk>
2017-08-08 17:30:04 +0100

    added screenshots

--- src/test/java/Testing/DemoSiteTest.java
+++ src/test/java/Testing/DemoSiteTest.java
@@ -72,35 +72,47 @@ public class DemoSiteTest {
         String userNameAndPass = "783huu";
 
         wD.navigate().to("http://thedemosite.co.uk/");
+        test.log(Status.INFO, "Connected to site");
 
         homepage.clickLoginPage(wD);
+        test.log(Status.INFO, "Clicked sigh up page");
 
         addUser.enterUserName(userNameAndPass , wD);
 
         addUser.enterPassword(userNameAndPass, wD);
+        test.log(Status.INFO, "Enter user name and password");
 
         addUser.clickEnterButton(wD);
-
+        test.log(Status.INFO, "Clicked enter");
         addUser.clickLoginPageButton(wD);
+        test.log(Status.INFO, "Clicked login page");
 
         logIn.enterUserName(userNameAndPass, wD);
 
         logIn.enterPassword(userNameAndPass, wD);
+        test.log(Status.INFO, "Enter user name and password");
 
         logIn.clickEnter(wD);
+        test.log(Status.INFO, "Clicked enter");
 
-        test.log(Status.INFO, "Info level");
-        test.fail("Failed");
+
+        assertEquals("login failed", "**Successful Login**", logIn.getLoginStateText(wD));
+        test.log(Status.PASS, "Login success");
+
+
+
 
         try {
             test.log(Status.INFO,
-                    "Error Snapshot : " + test.addScreenCaptureFromPath(ScreenShot.take(wD, "test")));
+                    "Login Screenshot : "
+                     + test.addScreenCaptureFromPath(ScreenShot.take(wD, "test")));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-        assertEquals("login failed", "**Successful Login**", logIn.getLoginStateText(wD));
+
+
 
 
 
